f5d428c4251def2c8a2db2be135b140a9f60d66d
Pratham Khanal <prathamkhanal962@gmail.com>
2025-09-07 22:34:32 +0545

    not using it
    not using it so , gotta go

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,11 +1,11 @@
 return {
 	"ellisonleao/gruvbox.nvim",
-	priority = 1000,
+	-- priority = 1000,
 	config = function()
 		require("gruvbox").setup({
 			contrast = "hard",
 		})
-		vim.cmd("colorscheme gruvbox")
+		-- vim.cmd("colorscheme gruvbox")
 	end,
 	opts = ...,
 }
