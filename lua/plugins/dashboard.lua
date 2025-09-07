603fb20e13701b97a9f7920cf11bae28a0c3701a
Pratham Khanal <prathamkhanal962@gmail.com>
2025-09-07 15:22:51 +0545

    formatting

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,7 +1,7 @@
 return {
-  "nvimdev/dashboard-nvim",
-  opts = function()
-    local logo = [[
+	"nvimdev/dashboard-nvim",
+	opts = function()
+		local logo = [[
 ⠀⠀⠀⠀⢀⣤⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⣤⡀⠀⠀⠀⠀
 ⠀⠀⠀⠀⣾⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣷⠀⠀⠀⠀
 ⠀⣴⣶⣾⣿⣿⣿⣿⡋⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣷⣶⣄⠀
@@ -21,77 +21,76 @@ return {
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⠠⣿⣷⣶⠁⣌⣉⣛⠛⠘⣛⣋⡁⢨⣶⣶⣿⡸⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⢀⣴⣶⣦⣄⣤⣾⣿⣿⣿⡿⢰⣮⡉⠛⢠⣿⣿⣿⣿⢸⣿⣿⣿⠈⠟⢋⣩⡄⣿⣿⣿⣿⣷⣄⣠⣴⣶⣦⡀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠈⣿⣿⣿⣷⣶⣤⣤⣤⣤⣤⣤⣴⣶⣿⣿⣿⠁⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠘⠿⠿⣿⣿⣿⣿⣯⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⣿⣿⣿⣿⡿⠿⠟⠁⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠃⠀⠀⠀⠀⠸⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠘⠿⠿⣿⣿⣿⣿⣯⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⣿⣿⣿⣿⡿⠿⠟⠁⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠃⠀⠀⠀⠀⠸⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀
 ⠀⠀⠀⠀⠀⠀⠀⠙⠛⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠛⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠛⠋⠀⠀⠀⠀⠀⠀⠀
     ]]
 
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
+		logo = string.rep("\n", 8) .. logo .. "\n\n"
 
-    local opts = {
-      theme = "doom",
-      hide = {
-        statusline = true,
-      },
-      config = {
-        header = vim.split(logo, "\n"),
-        center = {
-          { action = require("telescope.builtin").find_files, desc = " Find File", icon = " ", key = "f" },
-          { action = "ene | startinsert", desc = " New File", icon = " ", key = "n" },
-          {
-            action = require("telescope.builtin").oldfiles,
-            desc = " Recent Files",
-            icon = " ",
-            key = "r",
-          },
-          { action = require("telescope.builtin").live_grep, desc = " Find Text", icon = " ", key = "g" },
-          {
-            action = function()
-              vim.api.nvim_input("<cmd>cd ~/.config/nvim<CR>")
-              vim.api.nvim_input("<cmd>edit ~/.config/nvim/init.lua<CR>")
-              vim.api.nvim_input("<cmd>Neotree filesystem reveal right<CR>")
-            end,
-            desc = " Config",
-            icon = " ",
-            key = "c",
-          },
-          -- { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
-          --  { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
-          { action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
-          {
-            action = function()
-              vim.api.nvim_input("<cmd>qa<cr>")
-            end,
-            desc = " Quit",
-            icon = " ",
-            key = "q",
-          },
-        },
-        footer = function()
-          local stats = require("lazy").stats()
-          local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-          return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
-        end,
-      },
-    }
+		local opts = {
+			theme = "doom",
+			hide = {
+				statusline = true,
+			},
+			config = {
+				header = vim.split(logo, "\n"),
+				center = {
+					{ action = require("telescope.builtin").find_files, desc = " Find File", icon = " ", key = "f" },
+					{ action = "ene | startinsert", desc = " New File", icon = " ", key = "n" },
+					{
+						action = require("telescope.builtin").oldfiles,
+						desc = " Recent Files",
+						icon = " ",
+						key = "r",
+					},
+					{ action = require("telescope.builtin").live_grep, desc = " Find Text", icon = " ", key = "g" },
+					{
+						action = function()
+							vim.api.nvim_input("<cmd>cd ~/.config/nvim<CR>")
+							vim.api.nvim_input("<cmd>edit ~/.config/nvim/init.lua<CR>")
+							vim.api.nvim_input("<cmd>Neotree filesystem reveal right<CR>")
+						end,
+						desc = " Config",
+						icon = " ",
+						key = "c",
+					},
+					-- { action = 'lua require("persistence").load()',              desc = " Restore Session", icon = " ", key = "s" },
+					--  { action = "LazyExtras",                                     desc = " Lazy Extras",     icon = " ", key = "x" },
+					{ action = "Lazy", desc = " Lazy", icon = "󰒲 ", key = "l" },
+					{
+						action = function()
+							vim.api.nvim_input("<cmd>qa<cr>")
+						end,
+						desc = " Quit",
+						icon = " ",
+						key = "q",
+					},
+				},
+				footer = function()
+					local stats = require("lazy").stats()
+					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+					return { "⚡ Neovim loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms .. "ms" }
+				end,
+			},
+		}
 
-    for _, button in ipairs(opts.config.center) do
-      button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
-      button.key_format = "  %s"
-    end
+		for _, button in ipairs(opts.config.center) do
+			button.desc = button.desc .. string.rep(" ", 43 - #button.desc)
+			button.key_format = "  %s"
+		end
 
-    if vim.o.filetype == "lazy" then
-      vim.api.nvim_create_autocmd("WinClosed", {
-        pattern = tostring(vim.api.nvim_get_current_win()),
-        once = true,
-        callback = function()
-          vim.schedule(function()
-            vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
-          end)
-        end,
-      })
-    end
-
-    return opts
-  end,
+		if vim.o.filetype == "lazy" then
+			vim.api.nvim_create_autocmd("WinClosed", {
+				pattern = tostring(vim.api.nvim_get_current_win()),
+				once = true,
+				callback = function()
+					vim.schedule(function()
+						vim.api.nvim_exec_autocmds("UIEnter", { group = "dashboard" })
+					end)
+				end,
+			})
+		end
+		return opts
+	end,
 }
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,41 +1,36 @@
---BUG: mason not working
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ruff", "pyright" },
-			})
-			vim.keymap.set("n", "<leader>cm", "<cmd>Mason<CR>", { desc = "Mason" })
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "ruff", "pyright" },
+      })
+      vim.keymap.set("n", "<leader>cm", "<cmd>Mason<CR>", { desc = "Mason" })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      lspconfig.lua_ls.setup({
 
-			--		lspconfig.pyright.setup({
-			--			capabilities = capabilities,
-			--		})
-			lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ruff.setup({
 
-				capabilities = capabilities,
-			})
-			lspconfig.ruff.setup({
-
-				capabilities = capabilities,
-			})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
-			vim.keymap.set("n", "<Esc>", vim.lsp.buf.clear_references, { desc = "Go to declaration" })
-			vim.diagnostic.config({virtual_text = true,	})
-		end,
-	},
+        capabilities = capabilities,
+      })
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+      vim.diagnostic.config({ virtual_text = true })
+      -- grr for references, gra for code actions, grn for rename , gri for implementation default
+    end,
+  },
 }
