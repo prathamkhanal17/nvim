447dc53c0dfb5d3b8df729380edf921ba2ac6853
Pratham Khanal <prathamkhanal962@gmail.com>
2025-09-08 11:01:15 +0545

    changed keymap
    chaney control s to save a buffer and formatted other stuff

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,12 +1,12 @@
 local map = vim.keymap.set
-map('i','jk','<Esc>', { desc = "press jk for escape" })
-map('n','<leader>w','<cmd>w<CR>', { desc = "write buffer" })
-map('n','<leader>q','<cmd>q<CR>', { desc = "quit buffer" })
-map('n','<leader>Q','<cmd>qa!<CR>', { desc = "force quit all" })
-map('n','<leader>-','<cmd>split<CR>', { desc = "split horizontally" })
-map('n','<leader>|','<cmd>vsplit<CR>', { desc = "split vertically" })
-map('n','<leader>wl','<C-w>l', { desc = "Switch focus to right window" })
-map('n','<leader>wj','<C-w>j', { desc = "Switch focus to lower window" })
-map('n','<leader>wk','<C-w>k', { desc = "Switch focus to upper window" })
-map('n','<leader>wh','<C-w>h', { desc = "Switch focus to left window" })
--- TODO: Add some more generic keymaps 
+map('i', 'jk', '<Esc>', { desc = "press jk for escape" })
+map({ 'n', 'i' }, '<C-s>', '<cmd>w<CR>', { desc = "write buffer" })
+map('n', '<leader>q', '<cmd>q<CR>', { desc = "quit buffer" })
+map('n', '<leader>Q', '<cmd>qa!<CR>', { desc = "force quit all" })
+map('n', '<leader>-', '<cmd>split<CR>', { desc = "split horizontally" })
+map('n', '<leader>|', '<cmd>vsplit<CR>', { desc = "split vertically" })
+map('n', '<leader>wl', '<C-w>l', { desc = "Switch focus to right window" })
+map('n', '<leader>wj', '<C-w>j', { desc = "Switch focus to lower window" })
+map('n', '<leader>wk', '<C-w>k', { desc = "Switch focus to upper window" })
+map('n', '<leader>wh', '<C-w>h', { desc = "Switch focus to left window" })
+-- TODO: Add some more generic keymaps
