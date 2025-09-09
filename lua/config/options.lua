8171c9d46b11dcd827f418819abbf17f72513f6e
Pratham Khanal <prathamkhanal962@gmail.com>
2025-09-09 12:42:08 +0545

    number autocmds
    line number changes based on mode, rel for normal and absolute for
    insert mode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,19 @@ vim.cmd("set tabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set expandtab")
 vim.cmd("set clipboard=unnamedplus")
+
+-- Set line numbers based on mode
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+
+vim.api.nvim_create_autocmd({ "InsertEnter" }, {
+  callback = function()
+    vim.cmd("set norelativenumber")
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "InsertLeave" }, {
+  callback = function()
+    vim.cmd("set relativenumber")
+  end,
+})
